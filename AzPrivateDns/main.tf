108ccd7e2663e95fad2491e67cfae5607d9d987b
Eric van Wijk <ericvan@microsoft.com>
2019-12-17 13:21:45 +0000

    multiple interpreter options

--- AzPrivateDns/main.tf
+++ AzPrivateDns/main.tf
@@ -3,4 +3,12 @@ resource null_resource nested_pwsh {
     command                    = "script.ps1"
     interpreter                = ["pwsh", "-nop", "-File"]
   }
+  provisioner "local-exec" {
+    command                    = "./script.ps1"
+    interpreter                = ["pwsh", "-nop", "-Command"]
+  }
+  provisioner "local-exec" {
+    command                    = "Get-Command 'New-AzPrivateDnsRecordConfig'"
+    interpreter                = ["pwsh", "-nop", "-Command"]
+  }
 }
