1571b0e66e6d57ad0558c29ad1b19be5918f743b
Eric van Wijk <ericvan@microsoft.com>
2019-12-17 13:59:28 +0000

    More options

--- AzPrivateDns/main.tf
+++ AzPrivateDns/main.tf
@@ -1,4 +1,16 @@
-resource null_resource nested_pwsh_1 {
+resource null_resource nested_pwsh_1a {
+  provisioner "local-exec" {
+    command                    = "Get-Command 'Get-AzResource'"
+    interpreter                = ["pwsh", "-nop", "-Command"]
+  }
+
+  provisioner "local-exec" {
+    command                    = "Get-Command 'New-AzPrivateDnsRecordConfig'"
+    interpreter                = ["pwsh", "-nop", "-Command"]
+  }
+}
+
+resource null_resource nested_pwsh_1b {
   provisioner "local-exec" {
     command                    = "Get-Command 'Get-AzResource'"
     interpreter                = ["pwsh", "-nop", "-Command"]
@@ -12,21 +24,21 @@ resource null_resource nested_pwsh_1 {
 
 resource null_resource nested_pwsh_2 {
   # Doesn't work
-#   provisioner local-exec {
-#     command                    = "script.ps1"
-#     interpreter                = ["pwsh", "-nop", "-File"]
-#   }
+  provisioner local-exec {
+    command                    = "script.ps1"
+    interpreter                = ["pwsh", "-nop", "-File"]
+  }
 
-#   # Doesn't work
-#   provisioner local-exec {
-#     command                    = ". ./script.ps1"
-#     interpreter                = ["pwsh", 
-#                                   "-NoLogo", 
-#                                   "-NoProfile", 
-#                                   "-NonInteractive", 
-#                                   "-ExecutionPolicy","Unrestricted", 
-#                                   "-Command"]
-#   }
+  # Doesn't work
+  provisioner local-exec {
+    command                    = ". ./script.ps1"
+    interpreter                = ["pwsh", 
+                                  "-NoLogo", 
+                                  "-NoProfile", 
+                                  "-NonInteractive", 
+                                  "-ExecutionPolicy","Unrestricted", 
+                                  "-Command"]
+  }
 
   # Works
   provisioner local-exec {
