5a05424ffe53754565209110b7b3998f3cda7cb0
Eric van Wijk <ericvan@microsoft.com>
2019-12-17 13:56:38 +0000

    both options

--- AzPrivateDns/main.tf
+++ AzPrivateDns/main.tf
@@ -1,33 +1,4 @@
-# resource null_resource nested_pwsh {
-#   # Doesn't work
-# #   provisioner local-exec {
-# #     command                    = "script.ps1"
-# #     interpreter                = ["pwsh", "-nop", "-File"]
-# #   }
-
-# #   # Doesn't work
-# #   provisioner local-exec {
-# #     command                    = ". ./script.ps1"
-# #     interpreter                = ["pwsh", 
-# #                                   "-NoLogo", 
-# #                                   "-NoProfile", 
-# #                                   "-NonInteractive", 
-# #                                   "-ExecutionPolicy","Unrestricted", 
-# #                                   "-Command"]
-# #   }
-
-#   # Works
-#   provisioner local-exec {
-#     command                    = "Get-Command Get-AzResource"
-#     interpreter                = ["pwsh", "-nop", "-Command"]
-#   }
-#   provisioner local-exec {
-#     command                    = "Get-Command New-AzPrivateDnsRecordConfig"
-#     interpreter                = ["pwsh", "-nop", "-Command"]
-#   }
-# }
-
-resource null_resource nested_pwsh {
+resource null_resource nested_pwsh_1 {
   provisioner "local-exec" {
     command                    = "Get-Command 'Get-AzResource'"
     interpreter                = ["pwsh", "-nop", "-Command"]
@@ -35,6 +6,35 @@ resource null_resource nested_pwsh {
 
   provisioner "local-exec" {
     command                    = "Get-Command 'New-AzPrivateDnsRecordConfig'"
+    interpreter                = ["pwsh", "-nop", "-Command"]
+  }
+}
+
+resource null_resource nested_pwsh_2 {
+  # Doesn't work
+#   provisioner local-exec {
+#     command                    = "script.ps1"
+#     interpreter                = ["pwsh", "-nop", "-File"]
+#   }
+
+#   # Doesn't work
+#   provisioner local-exec {
+#     command                    = ". ./script.ps1"
+#     interpreter                = ["pwsh", 
+#                                   "-NoLogo", 
+#                                   "-NoProfile", 
+#                                   "-NonInteractive", 
+#                                   "-ExecutionPolicy","Unrestricted", 
+#                                   "-Command"]
+#   }
+
+  # Works
+  provisioner local-exec {
+    command                    = "Get-Command Get-AzResource"
+    interpreter                = ["pwsh", "-nop", "-Command"]
+  }
+  provisioner local-exec {
+    command                    = "Get-Command New-AzPrivateDnsRecordConfig"
     interpreter                = ["pwsh", "-nop", "-Command"]
   }
 }
