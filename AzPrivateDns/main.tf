810fb614e0c099ad869ecf03d26b8ac7f150f8cf
Eric van Wijk <ericvan@microsoft.com>
2019-12-17 12:38:58 +0000

    Addtional commandlet to compare to

--- AzPrivateDns/main.tf
+++ AzPrivateDns/main.tf
@@ -1,6 +1,11 @@
 resource null_resource nested_pwsh {
   provisioner "local-exec" {
+    command                    = "Get-Command 'Get-AzResource'"
+    interpreter                = ["pwsh", "-Command"]
+  }
+
+  provisioner "local-exec" {
     command                    = "Get-Command 'New-AzPrivateDnsRecordConfig'"
-    interpreter                = ["pwsh", "-nop", "-Command"]
+    interpreter                = ["pwsh", "-Command"]
   }
 }
