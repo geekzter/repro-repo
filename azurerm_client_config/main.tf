cfac52097568c9e69d318d16cf2370734a0a4566
Eric van Wijk <eric@van-wijk.com>
2020-03-31 16:41:07 +0200

    Draft issue

--- azurerm_client_config/main.tf
+++ azurerm_client_config/main.tf
@@ -1,6 +1,1 @@
 data azurerm_client_config current {}
-
-locals {
-    object_id = data.azurerm_client_config.current.object_id
-    tenant_id = data.azurerm_client_config.current.tenant_id
-}
--- azurerm_client_config/output.tf
+++ azurerm_client_config/output.tf
@@ -1,6 +1,6 @@
 output client_object_id {
-    value = local.object_id
+    value = data.azurerm_client_config.current.object_id
 }
 output client_tenant_id {
-    value = local.tenant_id
+    value = data.azurerm_client_config.current.tenant_id
 }
