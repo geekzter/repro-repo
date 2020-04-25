287dc00ad0e629af195b39499ac968596f9a1308
Eric van Wijk <eric@van-wijk.com>
2020-04-25 10:43:56 +0200

    object_id via data external -> azure cli

--- azurerm_client_config/main.tf
+++ azurerm_client_config/main.tf
@@ -1,1 +1,14 @@
 data azurerm_client_config current {}
+
+data external account_info {
+  program                      = [
+                                 "az",
+                                 "ad",
+                                 "signed-in-user",
+                                 "show",
+                                 "--query",
+                                 "{object_id:objectId}",
+                                 "-o",
+                                 "json",
+                                 ]
+}
--- azurerm_client_config/output.tf
+++ azurerm_client_config/output.tf
@@ -4,3 +4,7 @@ output client_object_id {
 output client_tenant_id {
     value = data.azurerm_client_config.current.tenant_id
 }
+
+output user_object_id {
+    value = data.external.account_info.result.object_id
+}
